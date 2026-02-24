5ba9a18f449a35d2c6e8ce3c7536c7b2248a067d
Robert Rendler <rendler@gmail.com>
2026-02-24 20:00:09 +1100

    feat(receipts): register receipt cropping route - added POST /receipts/crop/:id to handle client-side crop updates

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -437,6 +437,7 @@ sub startup {
     $family->post('/receipts/update/:id')->to('receipts#update');
     $family->post('/receipts/delete/:id')->to('receipts#delete');
     $family->get('/receipts/serve/:id')->to('receipts#serve');
+    $family->post('/receipts/crop/:id')->to('receipts#crop');
     $family->post('/receipts/ocr/:id')->to('receipts#trigger_ocr');
 
         # --- Chess Routes ---
