fd91b5cd4a9ea1fd025ca335752aae0bb9828303
Robert Rendler <rendler@gmail.com>
2026-02-24 15:32:47 +1100

    feat(receipts): add metadata update route for existing receipts

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -431,6 +431,7 @@ sub startup {
     $family->get('/receipts')->to('receipts#index');
     $family->get('/receipts/upload')->to('receipts#upload_form');
     $family->post('/receipts')->to('receipts#upload');
+    $family->post('/receipts/update/:id')->to('receipts#update');
     $family->post('/receipts/delete/:id')->to('receipts#delete');
     $family->get('/receipts/serve/:id')->to('receipts#serve');
 
