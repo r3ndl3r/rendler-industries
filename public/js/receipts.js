cad98dfe863f4e955ca19f4cab8abe50e899cea7
Robert Rendler <rendler@gmail.com>
2026-04-03 10:46:43 +1100

    refactor(receipts): remove redundant escapehtml implementation

--- public/js/receipts.js
+++ public/js/receipts.js
@@ -509,17 +509,6 @@ function getStoreLogoHtml(name) {
 }
 
 /**
- * Sanitizes input for DOM safety.
- * 
- * @param {string} text - Unsafe input.
- * @returns {string} - Escaped output.
- */
-function escapeHtml(text) {
-    if (!text) return '';
-    return String(text).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
-}
-
-/**
  * --- Image Refinement & AI Digitization ---
  */
 
